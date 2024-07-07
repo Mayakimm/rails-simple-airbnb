ccccfe32cfa901cf1db6b274b541f4fdecf7054f
Maya Kim <mayagy.kim@gmail.com>
2024-07-07 20:07:20 +0200

    Done show, index, new, create method

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,39 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Stylish House Close to River Thames',
+  address: '5 Riverside London SW15 1LN',
+  description: 'Enjoy the scenic view of River Thames from the comfort of this stylish house. Three bedrooms, modern kitchen, and a cozy living area.',
+  price_per_night: 100,
+  number_of_guests: 4
+)
+
+Flat.create!(
+  name: 'Cozy Studio in the Heart of Paris',
+  address: '12 Rue de Rivoli Paris 75001',
+  description: 'A cozy studio located in the heart of Paris. Close to all the major attractions, with a comfortable bed and a small kitchen.',
+  price_per_night: 65,
+  number_of_guests: 2
+)
+
+Flat.create!(
+  name: 'Modern Apartment in New York',
+  address: '20 West 34th Street New York, NY 10118',
+  description: 'A modern apartment with stunning views of the city. Features two bedrooms, a spacious living area, and a fully equipped kitchen.',
+  price_per_night: 150,
+  number_of_guests: 4
+)
+
+Flat.create!(
+  name: 'Patati patata the dream house in Nantes',
+  address: '12 Rue menou Nantes 44000',
+  description: 'A well made appartement which is near the center and not expensive at all. That is what we all want isnt it?',
+  price_per_night: 80,
+  number_of_guests: 10
+)
